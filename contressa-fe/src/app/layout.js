3f0966e2802df1efb13f96ccc4103f76bdc12247
Ebrahim <ebrahim@victopialabs.com>
2025-02-07 00:23:30 +0530

    introduced org context

--- contressa-fe/src/app/layout.js
+++ contressa-fe/src/app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import Navbar from '@/components/level-3/navbar';
-
+import Navbar from '@/components/level-3/Navbar';
+import { OrgProvider } from '@/context/OrgContext';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -9,8 +9,10 @@ export default function RootLayout({ children }) {
         <title>Contessa</title>
       </head>
       <body className="font-sans antialiased h-screen flex flex-col">
-        <Navbar />
-        {children}
+        <OrgProvider>
+          <Navbar />
+          <div className="mt-[60px]">{children}</div>
+        </OrgProvider>
       </body>
     </html>
   );
