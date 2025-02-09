bde415a36d05bb43da909c38ed6d94cf054499ac
Ebrahim <ebrahim@victopialabs.com>
2025-02-09 21:16:15 +0530

    corheader changes in settings.

--- contressa/settings.py
+++ contressa/settings.py
@@ -50,9 +50,11 @@ INSTALLED_APPS = [
     'organization',
     'authentication',
     'attendance',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -198,7 +200,12 @@ else:
     INSTALLED_APPS += ['debug_toolbar']
     MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
 
-    CORS_ALLOW_ALL_ORIGINS = True
+    CORS_ALLOWED_ORIGINS = [
+        "http://localhost:3000",  
+        "http://127.0.0.1:3000",
+    ]
+
+    CORS_ALLOW_CREDENTIALS = True
 
     LOGGING = {
         'version': 1,
